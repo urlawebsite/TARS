e845889ad37ee141e4fbc2d5f7fe51d1c61ce1b5
uzrivera <uzrivera@iu.edu>
2022-10-08 01:03:19 -0400

    added the main file

---
+++ main.py
@@ -0,0 +1,11 @@
+user_input = input()
+
+while user_input != "Goodbye TARS" or "Goodbye":
+    if user_input == "Hello TARS" or "Hello" or "hi" or "Hi":
+        print("Hello")
+        user_input = input()
+    if user_input == "Who are you" or "Who are you":
+        print("I am TARS")
+        user_input = input()
+
+print("Goodbye")
